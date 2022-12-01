38d6a2683e51cf501b8f4a6eeaf2011cf74acd5c
thelmaakosa <thelmaakosa107@gmail.com>
2022-12-01 13:58:41 -0500

    form validation update

--- assets/custom.js
+++ assets/custom.js
@@ -2,8 +2,8 @@ let myVar = setInterval(myTimer ,100);
 
 function myTimer() {
     console.log("1");
-
-    if ($(".product-section .product-single__form .pplr-recipient-email").length && $(".product-section .product-single__form .pplr-sales-rep-email").length){
+   
+    if ($(".product-section .product-single__form .pplr-sales-rep-email").length || $(".product-section .product-single__form .pplr-sales-rep-email-address").length){
         email_validation();
         $(".product-section .product-single__form button[type='submit']").appendTo(".product-section .product-single__form")
         clearInterval(myVar);
@@ -34,6 +34,19 @@ function email_validation(){
         if (IsEmail($(this).val()) == false ){
             $(".pplr-sales-rep-email").addClass("pplr_red_wrapper");
             $(".pplr-sales-rep-email").append("<small class='email-error' style='color:red;'>Email is not valid</small>");
+            $(".product-section .product-single__form button[type='submit']").attr("disabled", "true").css({"opacity":0.5, "background":"#2e373e", "color":"#FFF"});
+        }
+    })
+    $(".pplr-sales-rep-email-address input").focusin(function() {
+        $(".pplr-sales-rep-email-address").removeClass("pplr_red_wrapper")
+        $(".pplr-sales-rep-email-address small.email-error").remove();
+        $(".product-section .product-single__form button[type='submit']").removeAttr("disabled").css("opacity", 1);
+    });
+
+    $(".pplr-sales-rep-email-address input").focusout(function() {
+        if (IsEmail($(this).val()) == false ){
+            $(".pplr-sales-rep-email-address").addClass("pplr_red_wrapper");
+            $(".pplr-sales-rep-email-address").append("<small class='email-error' style='color:red;'>Email is not valid</small>");
             $(".product-section .product-single__form button[type='submit']").attr("disabled", "true").css({"opacity":0.5, "background":"#2e373e", "color":"#FFF"});
         }
     })
